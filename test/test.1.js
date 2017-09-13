06d73fc99b1f1ecf79816236d77b3eb2051c1263
Yogesh Jadhav <yogeshjadhav@Yogeshs-MacBook-Air.local>
2017-09-13 12:26:23 +0530

    Suggested logging format for logging Error object

--- test/test.1.js
+++ test/test.1.js
@@ -11,5 +11,15 @@ logger.tag('myTagB').debug({log: {
 	}
 });
 
+// This does not work due to issue at: https://github.com/trentm/node-bunyan/issues/369
 var error = new Error('the earth is flat');
 logger.error(error);
+
+// Whereas explicitly stating the object below works with one caveat i.e name property overrides the logger name property!
+var explicitError = new Error();
+explicitError.message = 'the earth is round :p';
+explicitError.name = 'discovery';   
+logger.error(explicitError);
+
+// It is therefore recommended to use the following format! This way we don't override name property of bunyan.
+logger.error({err: explicitError});
