0d03bb6810c0e96a5bc6787a9a992004c52766cb
Yogesh Jadhav <yogeshjadhav@Yogeshs-MacBook-Air.local>
2017-09-20 05:53:06 +0530

    Added a local test file which is used for testing outside docker environment

---
+++ test/test.local.1.js
@@ -0,0 +1,34 @@
+var logger = require('./../src/bunyanWrapper');
+
+
+logger.info({log: { message: "hi"}});
+logger.debug({log: {message: 'Your string here...'}});
+logger.tag('myTagA').debug({log: { message: 'Successfully connected' }});
+
+var planets = {planets: ['mars', 'earth']};
+logger.tag('myTagB').debug({log: {
+    type: 'AUDIT',
+    habitable: planets,
+	}
+});
+
+// This does not work due to issue at: https://github.com/trentm/node-bunyan/issues/369
+//var error = new Error('the earth is flat');
+//logger.error(error);
+
+// Whereas explicitly stating the object below works with one caveat i.e name property overrides the logger name property!
+console.log("\n\nIf we use logger.error({message: 'Your message', name: 'error name', stack: 'some stack....'});");
+console.log('It will override the bunyan name property, so such usage is discouraged. See below output for such behavior, name property is discovery instead of sp-json-logger');
+var explicitError = new Error();
+explicitError.message = 'the earth is round :p';
+explicitError.name = 'discovery';   
+explicitError.stack = 'Some stack here.....';
+logger.error(explicitError);
+
+console.log("\n\nUsing correct format below logger.error({err: object}), thus name isn't overriden");
+// It is therefore recommended to use the following format! This way we don't override name property of bunyan.
+logger.error({err: explicitError});
+
+
+
+
